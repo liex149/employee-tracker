a7ce47f94685477632a55e305bd0cc92bd6e2197
Linh Lien <lienx149@gmail.com>
2023-09-02 21:58:44 -0500

    added readme

--- server.js
+++ server.js
@@ -9,19 +9,36 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // TODO: Add MySQL password here
-        password: '',
-        database: 'company_db'
-      },
-      console.log(`Connected to the company_db database.`)
-    );
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         // MySQL username,
+//         user: 'root',
+//         // TODO: Add MySQL password here
+//         password: '',
+//         database: 'company_db'
+//       },
+//       console.log(`Connected to the company_db database.`)
+//     );
+// Query database
+db.query('SELECT * FROM students', function (err, results) {
+    console.log(results);
+});
+// read departments
+// Read all movies
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT id, employee_name AS name FROM department`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
 
-    app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  );;
