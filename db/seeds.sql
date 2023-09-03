3be3ebc9ff85abd857581791d95e7bab7f3c2932
Linh Lien <lienx149@gmail.com>
2023-09-02 20:21:21 -0500

    added server.js, schema.sql, seeds.sql, installed inquirer, installed mysql2

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Create new databases --
+CREATE DATABASE company_db;
+
+-- use company_db and makes it so all of the following cose will affect comapny_db--
+USE company_db;
+
+-- see databases in use --
+
